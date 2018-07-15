c9fa9fdd48712dfb3f27fe6d23206e931e2a0834
stsuta <s_tsutada@blogwatcher.co.jp>
2018-07-15 22:34:26 +0900

    Before each list shoud have its own url

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 # Create your models her.
+
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
+
