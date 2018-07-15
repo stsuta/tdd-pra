8d1c41b7fac936af3f7db76dca4dbce88648ea71
stsuta <s_tsutada@blogwatcher.co.jp>
2018-07-15 11:59:55 +0900

    new URL view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,23 @@ from django.test import TestCase
 from lists.models import Item
 
 # Create your tests here.
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('lists/the-only-list-in-the-world')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+
+
 class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
@@ -28,21 +45,13 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
