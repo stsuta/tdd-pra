b03f3b61e3989a66ce6bf162d3f26c84c6937247
stsuta <s_tsutada@blogwatcher.co.jp>
2018-07-17 01:03:59 +0900

    adfadfadfasdf

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n50o8mf)7bupq2&3zl7s&sw@^l@xc0##&^9w3t=oyfbu5j%x71'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
